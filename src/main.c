045494c86898fe59a1cc105d2c6d750ea564f68f
Samuel Marco <samuelmarco.2020@alunos.utfpr.edu.br>
2024-01-25 13:01:29 -0300

    feat: Validation and formatting of customer cpf and phone fields

--- src/main.c
+++ src/main.c
@@ -7,6 +7,10 @@
 
 #define MAX_LENGTH 50
 #define MIN_LENGTH 5
+#define LENGTH_INPUT 11
+#define LENGTH_SINGLE 12
+#define LENGTH_CPF 15
+#define LENGTH_PHONE 16  
 
 typedef struct {
     char street[MAX_LENGTH];
@@ -26,8 +30,8 @@ typedef struct {
 typedef struct {
     char name[MAX_LENGTH];
     char email[MAX_LENGTH];
-    char cpf[MAX_LENGTH];
-    char phone[MAX_LENGTH];
+    char cpf[LENGTH_CPF];
+    char phone[LENGTH_PHONE];
     Adress adress;
     Date dateRegister;
 } Customer;
@@ -35,9 +39,15 @@ typedef struct {
 Customer *createCustomer();
 void setName(Customer *customer);
 void setEmail(Customer *customer);
+void setCpf(Customer *customer);
+void setPhone(Customer *customer);
+
+char *formatCpf(char *cpf);
+char *formatPhone(char *phone);
 
 int isValidRegex(const char *input, const char *pattern);
 int isValidLengthInput(const char *input);
+int isValidCpfAndPhone(const char *input);
 
 int main() {
     setlocale(LC_ALL, "Portuguese");
@@ -60,10 +70,6 @@ void setName(Customer *customer) {
     }
 }
 
-int isValidLengthInput(const char *input) {
-    return (strlen(input) > MAX_LENGTH || strlen(input) < MIN_LENGTH) ? 0 : 1;
-}
-
 void setEmail(Customer *customer) {
     char email[MAX_LENGTH];
     const char *patternEmail = "^[a-z0-9._]+@[a-z]+\\.[a-z]+\\.[a-z]{2,}$";
@@ -71,12 +77,64 @@ void setEmail(Customer *customer) {
     printf("Digite o e-mail do cliente: ");
     scanf("%[^\n]", email);
 
-    if(!isValidRegex(email, patternEmail) || !isValidInput(email)) {
+    if(!isValidRegex(email, patternEmail) || !isValidLengthInput(email)) {
         printf("O e-mail não confere com o padrão da expressão regular, tente novamente.\n");
         setEmail(customer); // Recursive function
     } else {
         strcpy(customer->email, email);
     }
+}
+
+void setCpf(Customer *customer) {
+    char cpf[LENGTH_SINGLE];
+
+    printf("Digite o CPF do cliente (Apenas números): ");
+    scanf("%[^\n]", cpf);
+
+    if(!isValidCpfAndPhone(cpf)) {
+        printf("O CPF deve ter %d caracteres.\n", LENGTH_INPUT);
+        setCpf(customer); // Recursive function
+    } else {
+        strcpy(customer->cpf, formatCpf(cpf));
+    }
+}
+
+void setPhone(Customer *customer) {
+    char phone[LENGTH_SINGLE];
+
+    printf("Digite o telefone do cliente com DDD (Apenas números): ");
+    scanf("%[^\n]", phone);
+
+    if(!isValidCpfAndPhone(phone)) {
+        printf("O telefone deve ter %d caracteres.\n", LENGTH_INPUT);
+        setPhone(customer); // Recursive function
+    } else {
+        strcpy(customer->phone, formatPhone(phone));
+    }
+}
+
+char *formatCpf(char *cpf) {
+    char *cpfFormatted = (char *) malloc(LENGTH_CPF);
+    
+    if(cpfFormatted == NULL) {
+        printf("Erro ao alocar memória.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    sprintf(cpfFormatted, "%s.%s.%s-%s", cpf, cpf + 3, cpf + 6, cpf + 9);
+    return cpfFormatted;
+}
+
+char *formatPhone(char *phone) {
+    char *phoneFormatted = (char *) malloc(LENGTH_PHONE);
+
+    if(phoneFormatted == NULL) {
+        printf("Erro ao alocar memória.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    sprintf(phoneFormatted, "(%s) %s-%s", phone, phone + 2, phone + 7);
+    return phoneFormatted;
 }
 
 int isValidRegex(const char *input, const char *pattern) {
@@ -93,9 +151,28 @@ int isValidRegex(const char *input, const char *pattern) {
     return (!value) ? 1 : 0;
 }
 
+int isValidLengthInput(const char *input) {
+    return (strlen(input) > MAX_LENGTH || strlen(input) < MIN_LENGTH) ? 0 : 1;
+}
+
+int isValidCpfAndPhone(const char *input) {
+    return (strlen(input) == LENGTH_INPUT) ? 1 : 0;
+}
+
 Customer *createCustomer() {
     Customer *customer = (Customer *) malloc(sizeof(Customer));
 
+    if(customer == NULL) {
+        printf("Erro ao alocar memória.\n");
+        exit(EXIT_FAILURE);
+    }
+
     setName(customer);
     setEmail(customer);
+    setCpf(customer);
+    setPhone(customer);
+
+    //TODO: Adicionar data de cadastro e endereço 
+
+    return customer;
 }
