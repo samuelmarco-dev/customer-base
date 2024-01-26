3b5ed07ef06680e7257000bc54b4e3b5169b8324
Samuel Marco <samuelmarco.2020@alunos.utfpr.edu.br>
2024-01-26 14:26:16 -0300

    fix: validation of the customer's name, email, CPF and telephone fields

--- src/main.c
+++ src/main.c
@@ -2,54 +2,59 @@
 #include <stdlib.h>
 #include <string.h>
 #include <regex.h>
+#include <ctype.h>
+#include <curl/curl.h>
+#include <json-c/json.h>
 #include <locale.h>
-#include <time.h>
+//#include <time.h>
 
 #define MAX_LENGTH 50
 #define MIN_LENGTH 5
+#define LENGTH_CEP 8
+#define MAX_CEP 10
 #define LENGTH_INPUT 11
-#define LENGTH_SINGLE 12
 #define LENGTH_CPF 15
 #define LENGTH_PHONE 16  
 
-typedef struct {
-    char street[MAX_LENGTH];
-    char number[MAX_LENGTH];
-    char neighborhood[MAX_LENGTH];
-    char city[MAX_LENGTH];
-    char state[MAX_LENGTH];
-    char cep[MAX_LENGTH];
-} Adress;
+// typedef struct {
+//     char street[MAX_LENGTH];
+//     //char number[MAX_LENGTH];
+//     char neighborhood[MAX_LENGTH];
+//     char city[MAX_LENGTH];
+//     char state[MAX_LENGTH];
+//     char cep[MAX_CEP];
+// } Address;
 
-typedef struct {
-    int day;
-    int month;
-    int year;
-} Date;
+// typedef struct {
+//     int day;
+//     int month;
+//     int year;
+// } Date;
 
 typedef struct {
     char name[MAX_LENGTH];
     char email[MAX_LENGTH];
     char cpf[LENGTH_CPF];
     char phone[LENGTH_PHONE];
-    Adress adress;
-    Date dateRegister;
+    // Address address;
+    // Date dateRegister;
 } Customer;
+
+// struct MemoryStruct {
+//     char *memory;
+//     size_t size;
+// };
 
 Customer *createCustomer();
 void setName(Customer *customer);
 void setEmail(Customer *customer);
 void setCpf(Customer *customer);
 void setPhone(Customer *customer);
-void setAdress(Customer *customer);
 
-Adress *createAdress();
-void setCep(Adress *adress);
-void setStreet(Adress *adress);
-void setNumber(Adress *adress);
-void setNeighborhood(Adress *adress);
-void setCity(Adress *adress);
-void setState(Adress *adress);
+// Address *createAddress();
+// static size_t WriteMemoryCallback(void *contents, size_t size, size_t nmemb, void *userp);
+// int searchByCEP(const char *cep, Address *address);
+// int isValidCep(const char *cep);
 
 char *formatCpf(char *cpf);
 char *formatPhone(char *phone);
@@ -57,69 +62,92 @@ char *formatPhone(char *phone);
 int isValidRegex(const char *input, const char *pattern);
 int isValidLengthInput(const char *input);
 int isValidCpfAndPhone(const char *input);
+int isNumbers(const char *input);
 
 int main() {
     setlocale(LC_ALL, "Portuguese");
+    Customer *dataBase[1];
+    
+    dataBase[0] = createCustomer();
+    printf("Cliente cadastrado com sucesso!\n");
+
+    free(dataBase[0]);
     
     return 0;
 }
 
 void setName(Customer *customer) {
     char name[MAX_LENGTH];
-    const char *patternName = "^[A-Za-zÀ-ÖØ-öø-ÿ]+(?: [A-Za-zÀ-ÖØ-öø-ÿ]+)*$";
+    const char *patternName = "^[a-zA-Z]+( [a-zA-Z]+)*$";
 
-    printf("Digite o nome do cliente: ");
-    scanf("%[^\n]", name);
+    do {
+        printf("Digite o nome do cliente: ");
+        scanf("%50[^\n]", name);
+        getchar();
+        
+        if(!isValidLengthInput(name))
+            printf("O nome deve ter entre %d e %d caracteres.\n", MIN_LENGTH, MAX_LENGTH);
+         else if(!isValidRegex(name, patternName)) 
+            printf("O nome não confere com o padrão da expressão regular, tente novamente.\n");
+         else break;
+    } while(!(isValidLengthInput(name) && isValidRegex(name, patternName)));
 
-    if(!isValidRegex(name, patternName) || !isValidLengthInput(name)) {
-        printf("O nome do cliente deve ter no mínimo %d caracteres e no máximo %d caracteres.\n", MIN_LENGTH, MAX_LENGTH);
-        setName(customer); // Recursive function
-    } else {
-        strcpy(customer->name, name);
-    }
+    strcpy(customer->name, name);
 }
 
 void setEmail(Customer *customer) {
     char email[MAX_LENGTH];
-    const char *patternEmail = "^[a-z0-9._]+@[a-z]+\\.[a-z]+\\.[a-z]{2,}$";
+    const char *patternEmail = "^[a-z0-9._]+@[a-z]+\\.[a-z]{3,}$";
 
-    printf("Digite o e-mail do cliente: ");
-    scanf("%[^\n]", email);
+    do {
+        printf("Digite o e-mail do cliente (.com): ");
+        scanf("%50[^\n]", email);
+        getchar();
 
-    if(!isValidRegex(email, patternEmail) || !isValidLengthInput(email)) {
-        printf("O e-mail não confere com o padrão da expressão regular, tente novamente.\n");
-        setEmail(customer); // Recursive function
-    } else {
-        strcpy(customer->email, email);
-    }
+        if(!isValidLengthInput(email))
+            printf("O e-mail deve ter entre %d e %d caracteres.\n", MIN_LENGTH, MAX_LENGTH);
+        else if(!isValidRegex(email, patternEmail)) 
+            printf("O e-mail não confere com o padrão da expressão regular, tente novamente.\n");
+        else break;
+    } while(!(isValidLengthInput(email) && isValidRegex(email, patternEmail)));
+
+    strcpy(customer->email, email);
 }
 
 void setCpf(Customer *customer) {
-    char cpf[LENGTH_SINGLE];
+    char cpf[LENGTH_INPUT];
 
-    printf("Digite o CPF do cliente (Apenas números): ");
-    scanf("%[^\n]", cpf);
+    do {
+        printf("Digite o CPF do cliente (Apenas números): ");
+        scanf("%11[^\n]", cpf);
+        getchar();
 
-    if(!isValidCpfAndPhone(cpf)) {
-        printf("O CPF deve ter %d caracteres.\n", LENGTH_INPUT);
-        setCpf(customer); // Recursive function
-    } else {
-        strcpy(customer->cpf, formatCpf(cpf));
-    }
+        if(!isValidCpfAndPhone(cpf))
+            printf("O CPF deve ter %d caracteres.\n", LENGTH_INPUT);
+        else if(!isNumbers(cpf))
+            printf("O CPF deve conter apenas números.\n");
+        else break;
+    } while(!(isValidCpfAndPhone(cpf) && isNumbers(cpf)));
+    
+    strcpy(customer->cpf, formatCpf(cpf));
 }
 
 void setPhone(Customer *customer) {
-    char phone[LENGTH_SINGLE];
+    char phone[LENGTH_INPUT];
 
-    printf("Digite o telefone do cliente com DDD (Apenas números): ");
-    scanf("%[^\n]", phone);
+    do{
+        printf("Digite o telefone do cliente com DDD (Apenas números): ");
+        scanf("%11[^\n]", phone);
+        getchar();
 
-    if(!isValidCpfAndPhone(phone)) {
-        printf("O telefone deve ter %d caracteres.\n", LENGTH_INPUT);
-        setPhone(customer); // Recursive function
-    } else {
-        strcpy(customer->phone, formatPhone(phone));
-    }
+        if(!isValidCpfAndPhone(phone)) 
+            printf("O telefone deve ter %d caracteres.\n", LENGTH_INPUT);
+        else if(!isNumbers(phone))
+            printf("O telefone deve conter apenas números.\n");
+        else break;
+    } while(!(isValidCpfAndPhone(phone) && isNumbers(phone)));
+
+    strcpy(customer->phone, formatPhone(phone));
 }
 
 char *formatCpf(char *cpf) {
@@ -130,7 +158,7 @@ char *formatCpf(char *cpf) {
         exit(EXIT_FAILURE);
     }
 
-    sprintf(cpfFormatted, "%s.%s.%s-%s", cpf, cpf + 3, cpf + 6, cpf + 9);
+    snprintf(cpfFormatted, LENGTH_CPF, "%.*s.%.*s.%.*s-%.*s", 3, cpf, 3, cpf + 3, 3, cpf + 6, 2, cpf + 9);
     return cpfFormatted;
 }
 
@@ -142,11 +170,11 @@ char *formatPhone(char *phone) {
         exit(EXIT_FAILURE);
     }
 
-    sprintf(phoneFormatted, "(%s) %s-%s", phone, phone + 2, phone + 7);
+    snprintf(phoneFormatted, LENGTH_PHONE, "(%.*s) %.*s-%.*s", 2, phone, 5, phone + 2, 4, phone + 7);
     return phoneFormatted;
 }
 
-int isValidRegex(const char *input, const char *pattern) {
+int isValidRegex(const char *input, const char *pattern) {    
     regex_t regex;
     int value; 
 
@@ -154,10 +182,12 @@ int isValidRegex(const char *input, const char *pattern) {
         fprintf(stderr, "Não foi possível compilar a expressão regular\n");
         exit(EXIT_FAILURE);
     }
-    value = regexec(&regex, input, (size_t) 0, NULL, 0);
+
+    fprintf(stderr, "Expressão regular compilada com sucesso\n");
+    value = regexec(&regex, input, (size_t) 0, (regmatch_t *) NULL, 0);
     regfree(&regex);
 
-    return (!value) ? 1 : 0;
+    return (value || value == REG_NOMATCH) ? 0 : 1;
 }
 
 int isValidLengthInput(const char *input) {
@@ -168,43 +198,152 @@ int isValidCpfAndPhone(const char *input) {
     return (strlen(input) == LENGTH_INPUT) ? 1 : 0;
 }
 
+int isNumbers(const char *input) {
+    for (int i = 0; input[i] != '\0'; i++) {
+        if (!isdigit(input[i])) return 0;
+    }
+
+    return 1;
+}
+
 Customer *createCustomer() {
     Customer *customer = (Customer *) malloc(sizeof(Customer));
 
     if(customer == NULL) {
-        printf("Erro ao alocar memória.\n");
+        fprintf(stderr, "Erro: Alocação de memória!\n");
         exit(EXIT_FAILURE);
     }
+    
+    // setName(customer);
+    // setEmail(customer);
+    //setCpf(customer);
+    //setPhone(customer);
 
-    setName(customer);
-    setEmail(customer);
-    setCpf(customer);
-    setPhone(customer);
-
-    //TODO: Adicionar data de cadastro e endereço 
+    // Address *address = createAddress();
+    // customer->address = *address;
 
     return customer;
 }
 
-void setCep(Adress *adress) {}
+// void setCep(Address *adress) {
+//     char cep[MAX_CEP];
 
-void setStreet(Adress *adress) {}
+//     printf("Digite o CEP do cliente (Apenas números): ");
+//     scanf("%[^\n]", cep);
 
-void setNumber(Adress *adress) {}
+//     if(!isValidCep(cep)) {
+//         printf("O CEP deve ter %d caracteres.\n", LENGTH_CEP);
+//         setCep(adress); // Recursive function
+//     } else {
+//         strcpy(adress->cep, cep);
+//         getchar();
+//     }
+// }
 
-void setNeighborhood(Adress *adress) {}
+// int isValidCep(const char *cep) {
+//     return (strlen(cep) == LENGTH_CEP) ? 1 : 0;
+// }
 
-void setCity(Adress *adress) {}
+// static size_t WriteMemoryCallback(void *contents, size_t size, size_t nmemb, void *userp) {
+//     size_t realsize = size *nmemb;
+//     struct MemoryStruct *mem = (struct MemoryStruct *)userp;
 
-void setState(Adress *adress) {}
+//     mem->memory = realloc(mem->memory, mem->size + realsize + 1);
+//     if (mem->memory == NULL) {
+//         printf("Erro ao alocar memória.\n");
+//         exit(EXIT_FAILURE);
+//     }
 
-Adress *createAdress() {
-    Adress *adress = (Adress *) malloc(sizeof(Adress));
+//     memcpy(&(mem->memory[mem->size]), contents, realsize);
+//     mem->size += realsize;
+//     mem->memory[mem->size] = 0;
 
-    if(adress == NULL) {
-        printf("Erro ao alocar memória.\n");
-        exit(EXIT_FAILURE);
-    }
+//     return realsize;
+// }
 
-    return adress;
-}
+// int searchByCEP(const char *cep, Address *address) {
+//     CURL *curl;
+//     CURLcode res;
+
+//     struct MemoryStruct chunk;
+
+//     chunk.memory = malloc(sizeof(char));
+//     chunk.size = 0;
+
+//     curl_global_init(CURL_GLOBAL_DEFAULT);
+//     curl = curl_easy_init();
+
+//     if(curl) {
+//         char url[MAX_LENGTH];
+//         sprintf(url, "https://brasilaberto.com/api/v1/zipcode/%s", cep);
+
+//         curl_easy_setopt(curl, CURLOPT_URL, url);
+//         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
+//         curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)&chunk);
+        
+//         res = curl_easy_perform(curl);
+
+//         if(res != CURLE_OK) {
+//             fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+//             return -1;
+//         }
+        
+//         json_object *json = json_tokener_parse(chunk.memory);
+//         if(json == NULL) {
+//             fprintf(stderr, "Erro ao analisar JSON\n");
+//             free(chunk.memory);
+//             curl_easy_cleanup(curl);
+//             return -1;
+//         }
+
+//         json_object *response;
+//         if(json_object_object_get_ex(json, "response", &response)) {
+//             json_object *street, *neighborhood, *city, *state;    
+
+//             if(json_object_object_get_ex(response, "street", &street)) {
+//                 strcpy(address->street, json_object_get_string(street));
+//                 address->street[strlen(address->street)] = '\0';
+//             }
+//             if(json_object_object_get_ex(response, "district", &neighborhood)) {
+//                 strcpy(address->neighborhood, json_object_get_string(neighborhood));
+//                 address->neighborhood[strlen(address->neighborhood)] = '\0';
+//             }
+//             if(json_object_object_get_ex(response, "city", &city)) {
+//                 strcpy(address->city, json_object_get_string(city));
+//                 address->city[strlen(address->city)] = '\0';
+//             }
+//             if(json_object_object_get_ex(response, "state", &state)) {
+//                 strcpy(address->state, json_object_get_string(state));
+//                 address->state[strlen(address->state)] = '\0';
+//             } 
+//         }
+
+//         json_object_put(json);
+//         free(chunk.memory);
+
+//         curl_easy_cleanup(curl);
+//         curl_global_cleanup();
+//         return 0; 
+//     } else {
+//         fprintf(stderr, "Erro ao inicializar o CURL\n");
+//         return -1;
+//     }
+// }
+
+// Address *createAddress() {
+//     Address *address = (Address *) malloc(sizeof(Address));
+
+//     if(address == NULL) {
+//         printf("Erro ao alocar memória.\n");
+//         exit(EXIT_FAILURE);
+//     }
+
+//     setCep(address);
+//     int result = searchByCEP(address->cep, address);
+
+//     if(result == -1) {
+//         printf("Erro ao buscar o CEP.\n");
+//     }
+
+//     return address;
+// }
